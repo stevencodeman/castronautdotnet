9322a0cf06089c8db1c84b4eb8e839e28c4af9b9
stevencodeman <stevencodeman@gmail.com>
2022-10-28 09:13:07 -0700

    album

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const menuToggle = document.querySelector('.toggle');
+const menuToggle = document.querySelector('.menuToggle');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => {
